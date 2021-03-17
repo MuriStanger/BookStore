1b1d55dc3402bc6a8743be8ce51ac28fe8a16ed9
MuriStanger <murielzanellato@gmail.com>
2021-03-17 18:29:55 -0300

    criando construtor

--- bookstore-app/src/app/autores/autores.page.ts
+++ bookstore-app/src/app/autores/autores.page.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Autor } from '../autor';
+import { Genero } from './genero.enum';
 
 @Component({
   selector: 'app-autores',
@@ -6,9 +8,29 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./autores.page.scss'],
 })
 export class AutoresPage implements OnInit {
+  autores: Autor[];
 
-  constructor() { }
-
+  constructor() 
+  {
+    this.autores = 
+    [
+      {
+        nome: 'David Flanagan',
+        dataNascimento: new Date(1980,11,13), 
+        genero:Genero.MASCULINO,
+      },
+      {
+        nome: 'Douglas Cockford',
+        dataNascimento: new Date(1975,5,17), 
+        genero:Genero.MASCULINO,
+      },
+      {
+        nome: 'Martin Fowler',
+        dataNascimento: new Date(1955,6,15), 
+        genero:Genero.MASCULINO,
+      }
+    ];
+  }
   ngOnInit() {
   }
 
